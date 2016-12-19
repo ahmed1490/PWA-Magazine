67ae4213dcb1ef98d46805c6df4ac252ba0f09b4
Atharva Johri <atharva.johri@zalando.de>
2016-12-19 12:55:15 +0100

    Added initial page and timeline components, updated react and react dom versions

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import Timeline from './components/Timeline';
 
 ReactDOM.render(
-  <App />,
+  <Timeline />,
   document.getElementById('root')
 );
