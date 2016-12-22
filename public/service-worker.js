ef4aff4ac9d6ab1b3d9221b37e0e0bec1985e514
ahmed1490 <ahmed1490+git@gmail.com>
2016-12-22 02:14:13 +0100

    fix caching for get items

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,12 +10,13 @@ const filesToCache = [
   './js/offline.js',
   './js/toast.js',
   '/cache-polyfill.js',
+  '/getNewsArticleLinks',
   './manifest.json'
 ];
 
 self.addEventListener('install', function (event) {
     event.waitUntil( //this ensures that the service worker will not install until the code inside waitUntil() has successfully occurred.
-        caches.open('z.fm-v1')
+        caches.open(cacheName)
         .then(function(cache) {
             return cache.addAll(filesToCache)
             .then(function () {
@@ -36,23 +37,23 @@ self.addEventListener('fetch', function (event) {
     event.respondWith(
       //Tell the browser to wait for network request and respond with below
       caches.match(request).then(function(response) {
-        console.log('response from cache', response);
+        console.info('response from cache', response);
         if (response) {
-          console.log('from cache url', response.url);
+          console.info('from cache url', response.url);
           return response;
         }
 
         //if request is not cached, add it to cache
-        console.log('fetching', request.url);
+        console.info('fetching', request.url);
         return fetch(request, {mode: 'no-cors'})
           .then((response) => {
               var responseToCache = response.clone();
 
               caches.open(cacheName).then((cache) => {
                   cache.put(request, responseToCache)
-                      .then(() => console.log('to cache', response.url))
+                      .then(() => console.info('to cache', response.url))
                       .catch(function(err) {
-                        console.warn(request.url + ': ' + err.message);
+                        console.error('error caching', request.url + ': ' + err.message);
                       });
                 });
 
@@ -62,6 +63,16 @@ self.addEventListener('fetch', function (event) {
     );
 });
 
+
+self.addEventListener('message', function handler (event) {
+  console.info('SW message', event.data);
+  if (event.data.command === 'cache') {
+    caches.open(cacheName)
+        .then(function(cache) {
+            return cache.addAll(event.data.urlsToCache)
+        });
+  }
+});
 
 
 /*
--- src/utils/articleRetriever.js
+++ src/utils/articleRetriever.js
@@ -17,7 +17,7 @@ function getArticleLinks() {
     })
 }
 
-function getArticle(articleLinkData, retries = 10) {
+function getArticle(articleLinkData, retries = 3) {
     return fetch(`/getNewsArticleDescription?articleLink=${encodeURIComponent(articleLinkData.link)}`)
         .then(response => {
             return response.json();
@@ -25,7 +25,8 @@ function getArticle(articleLinkData, retries = 10) {
         .then(responseJSON => {
             return { ...articleLinkData, descriptionFetched: true, ...responseJSON}
         })
-        .catch(() => {
+        .catch((err) => {
+            console.error('error in fetching description', err);
             if (retries > 0) {
                 getArticle(articleLinkData, retries-1);
             }
@@ -34,16 +35,26 @@ function getArticle(articleLinkData, retries = 10) {
 
 function getPagesFromIndex(index){
     return getArticleLinks().then((articleLinks) => {
-        articleData = articleLinks;
+        articleData = articleLinks.slice(0, 10);
         /*let startIndex = (index - 3) > 0 ? (index - 3) : 0;
         let endIndex = (index + 3) < articleData.length ? (index + 3) : articleData.length;*/
 
-        const articlePromises = articleData.slice(0, 10).map((articleLinkData) => {
+        cacheLinksToServiceWorker(articleData.map((articleData) => `/getNewsArticleDescription?articleLink=${encodeURIComponent(articleData.link)}`));
+
+        const articlePromises = articleData.map((articleLinkData) => {
             return (articleLinkData.descriptionFetched ? articleLinkData : getArticle(articleLinkData));
         });
 
         return Promise.all(articlePromises).then((updatedArticleData) => {
             return updatedArticleData;
+        });
+    });
+}
+
+function cacheLinksToServiceWorker(linksToCache) {
+    navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+        serviceWorkerRegistration.addEventListener('activate', function (event) {
+            navigator.serviceWorker.controller.postMessage({ command: 'cache', urlsToCache: linksToCache })
         });
     });
 }
